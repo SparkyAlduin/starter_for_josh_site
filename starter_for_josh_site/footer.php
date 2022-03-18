3fb49a3f4a5e3bb787bb36a412a97d5b88bec50e
SparkyAlduin <100830753+SparkyAlduin@users.noreply.github.com>
2022-03-18 19:58:52 +0800

    Create style.css
    Add the reference for WordPress

---
+++ starter_for_josh_site/footer.php
@@ -0,0 +1,25 @@
+    <footer class="container-fluid text-center">
+        <!-- div for the main image to strech to the edge of the page -->
+
+        <div class="container">
+            <!-- div to hold the content in the middle of the page -->
+
+            <div class="row">
+                <!-- a row that gives us access to the BS columns -->
+
+                <div class="col-md-6">
+                    <p>JOSH WHITKIN</p>
+                </div>
+
+                <div class="col-md-6">
+                    <p>Terms and Conditions</p>
+                </div>
+
+            </div> <!-- row -->
+
+        </div> <!-- container -->
+
+    </footer> <!-- container fluid -->
+<?php wp_footer(); ?>
+</body>
+</html>
